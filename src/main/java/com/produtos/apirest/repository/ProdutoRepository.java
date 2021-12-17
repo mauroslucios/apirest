15e4f81f98c3bab9123c8e18bc34787a9a7842a4
mauroslucios <mauroslucios@gmail.com>
2021-12-17 09:21:11 -0300

    Repository e Service criados

---
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.produtos.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.produtos.apirest.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
---
+++ src/main/java/com/produtos/apirest/service/ProdutoService.java
@@ -0,0 +1,11 @@
+package com.produtos.apirest.service;
+
+import org.springframework.stereotype.Service;
+
+import com.produtos.apirest.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	ProdutoRepository produtoRepository;
+}
