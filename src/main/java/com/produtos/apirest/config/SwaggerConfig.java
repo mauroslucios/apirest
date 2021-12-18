e9e614361e23ab94129cb92c0393a4387721e311
mauroslucios <mauroslucios@gmail.com>
2021-12-17 22:28:31 -0300

    Update Swagger

--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package com.produtos.apirest.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,12 +25,15 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_12)
+		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.produtos.apirest"))
-				.paths(PathSelectors.regex("/api.*"))
-				.build()
-				.apiInfo(metaInfo());
+		        .apis(RequestHandlerSelectors.any())
+		        .paths(PathSelectors.any())
+		        .build()
+		        .apiInfo(metaInfo())
+		        .useDefaultResponseMessages(false)
+		        .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
+				
 	}
 	
 	private ApiInfo metaInfo() {
@@ -30,10 +41,32 @@ public class SwaggerConfig {
 				.title("Produtos API REST")
 				.description("API REST de cadastro de produtos.")
 				.version("1.0")
+				.contact(new Contact("Mauro Lúcio", "https://linkedin/in/mauro-lúcio-pereira", "mauroslucios@gmail.com"))
 				.license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .build();
 	}
+	
+	private List<ResponseMessage> responseMessageForGET()
+	{
+	    return new ArrayList<ResponseMessage>() {/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+		{
+	        add(new ResponseMessageBuilder()
+	            .code(500)
+	            .message("500 message")
+	            .responseModel(new ModelRef("Error"))
+	            .build());
+	        add(new ResponseMessageBuilder()
+	            .code(403)
+	            .message("Forbidden!")
+	            .build());
+	    }};
+	}
+
 	
 }
 	
--- src/main/java/com/produtos/apirest/resource/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resource/ProdutoResource.java
@@ -18,6 +18,7 @@ import com.produtos.apirest.model.Produto;
 import com.produtos.apirest.service.ProdutoService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value="/api")
@@ -29,27 +30,32 @@ public class ProdutoResource {
 	private ProdutoService produtoService;
 	
 	@GetMapping("/produtos")
+	@ApiOperation(value="Retorna uma lista de produtos")
 	public List<ProdutoDTO> listaProdutos(){
 		return produtoService.findAll();
 	}
 	
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna um produto pelo id")
 	public Produto findById(@PathVariable(value="id") long id){
 		return produtoService.findById(id);
 	}
 	
 	@PostMapping("/produto")
+	@ApiOperation(value="Inseri um produto no banco")
 	public Produto insertProduto(@RequestBody Produto produto) {
 		return produtoService.insertProduto(produto);
 		
 	}
 	
 	@DeleteMapping("/produto/{id}")
+	@ApiOperation(value="Deleta um produto pelo id")
 	public void deleteProduto(@PathVariable long id) {
 		produtoService.deleteProdutoById(id);
 	}
 	
 	@PutMapping("/produto")
+	@ApiOperation(value="Atualiza um produto completo")
 	public Produto updateProduto(@RequestBody Produto produto) {
 		return produtoService.updateProdutoById(produto);
 	}
