7a157f3bb9ea3db41282e0292cb55d114c4b0663
mauroslucios <mauroslucios@gmail.com>
2021-12-17 21:01:11 -0300

    Adicionando Annotations Api e CrossOrigin

--- src/main/java/com/produtos/apirest/resource/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resource/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.produtos.apirest.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,12 @@ import com.produtos.apirest.dto.ProdutoDTO;
 import com.produtos.apirest.model.Produto;
 import com.produtos.apirest.service.ProdutoService;
 
+import io.swagger.annotations.Api;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins="*")
 public class ProdutoResource {
 
 	@Autowired
