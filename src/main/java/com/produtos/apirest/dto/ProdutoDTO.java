3f5ef33cf2905f93d6a12106cbb7e927d0edba78
mauroslucios <mauroslucios@gmail.com>
2021-12-23 18:26:53 -0300

    Adicionado docker-compose postgresql com pgadmin

--- src/main/java/com/produtos/apirest/dto/ProdutoDTO.java
+++ src/main/java/com/produtos/apirest/dto/ProdutoDTO.java
@@ -36,39 +36,17 @@ public class ProdutoDTO extends RepresentationModel<ProdutoDTO> implements Seria
 		return id;
 	}
 
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-
 	public String getNome() {
 		return nome;
 	}
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 
 	public BigDecimal getQuantidade() {
 		return quantidade;
 	}
 
-
-	public void setQuantidade(BigDecimal quantidade) {
-		this.quantidade = quantidade;
-	}
-
-
 	public BigDecimal getValor() {
 		return valor;
 	}
 
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-		
+	
 }
